f28101a04bdc4866322c341b35bb42b1e4e17f15
Lida Feoktistova <lida.feoktistova@mail.ru>
2024-06-03 01:03:16 +0300

    auth

--- apps/account/src/app/auth/auth.service.ts
+++ apps/account/src/app/auth/auth.service.ts
@@ -1,4 +1,48 @@
 import { Injectable } from '@nestjs/common';
+import { AccountRegister } from '@purple/contracts';
+import { UserRepository } from '../user/repositories/user.repository';
+import { UserEntity } from '../user/entities/user.entity';
+import { UserRole } from '@purple/interfaces';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private readonly userRepository: UserRepository,
+    private readonly jwtService: JwtService
+  ) {}
+
+  async register({ email, password, displayName }: AccountRegister.Request) {
+    const oldUser = await this.userRepository.findUser(email);
+    if (oldUser) {
+      throw new Error('User already exists');
+    }
+    const newUserEntity = await new UserEntity({
+      displayName,
+      email,
+      passwordHash: '',
+      role: UserRole.Student
+    }).setPassword(password);
+    const newUser = await this.userRepository.createUser(newUserEntity);
+    return { email: newUser.email };
+  }
+
+  async validateUser(email: string, password: string) {
+    const user = await this.userRepository.findUser(email);
+    if (!user) {
+      throw new Error('Login or password is incorrect');
+    }
+    const userEntity = new UserEntity(user);
+    const isCorrectPassword = await userEntity.validatePassword(password);
+    if (!isCorrectPassword) {
+      throw new Error('Login or password is incorrect');
+    }
+    return { id: user._id };
+  }
+
+  async login(id: string) {
+    return {
+      access_token: await this.jwtService.signAsync({ id })
+    };
+  }
+}
