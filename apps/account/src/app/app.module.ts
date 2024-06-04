a10390083f5259e410e1ca4617cbe91fe29f2e3e
Lida Feoktistova <lida.feoktistova@mail.ru>
2024-06-04 14:57:26 +0300

    add nestjs-rmq

--- apps/account/src/app/app.module.ts
+++ apps/account/src/app/app.module.ts
@@ -1,13 +1,16 @@
 import { Module } from '@nestjs/common';
+import { RMQModule } from 'nestjs-rmq';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { getMongoConfig } from './configs/mongo.config';
+import { getRMQConfig } from './configs/rmq.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true, envFilePath: 'envs/.account.env'}),
+    RMQModule.forRootAsync(getRMQConfig()),
     MongooseModule.forRootAsync(getMongoConfig()),
     UserModule,
     AuthModule
